03867a318a17998438370877c358d90d168be536
devfm <devfm.go@gmail.com>
2026-03-14 07:04:58 +0700

    feat: use env

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -2,14 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCu43vq9UrrBjpMOJtLAUQ8gHveP0Keqs4",
-  authDomain: "fast-campus-hotel-bookin-d6e7c.firebaseapp.com",
-  databaseURL:
-    "https://fast-campus-hotel-bookin-d6e7c-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "fast-campus-hotel-bookin-d6e7c",
-  storageBucket: "fast-campus-hotel-bookin-d6e7c.firebasestorage.app",
-  messagingSenderId: "999561679829",
-  appId: "1:999561679829:web:78c2d35169a61f1be1a35c",
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  databaseURL: import.meta.env.VITE_DATABASE_URL,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 // Initialize Firebase
